4f60995d7da0b2e3b59d9ac10dd6003d1507effe
Alexander Klochko <90823272+zernvaka@users.noreply.github.com>
2022-02-27 18:49:27 +0300

    Delete LW3 directory

--- LW3/Task1.php
+++
@@ -1,8 +0,0 @@
-<?php
-    header("Content-Type: text/plain");
-    $text = trim($_GET['text']);
-    while (strpos($text, '  ') != false)
-    {
-        $text = str_replace('  ',' ', $text);
-    }
-    echo $text;
