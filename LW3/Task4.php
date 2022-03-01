7dbe0aa33b06af1c966179e6833f0161288c1346
Alexander Klochko <90823272+zernvaka@users.noreply.github.com>
2022-03-01 23:43:15 +0300

    Третья лабораторная, четвертое задание

--- LW3/Task4.php
+++ LW3/Task4.php
@@ -1,27 +1,51 @@
 <?php
     header("Content-Type: text/plain");
-    $email = trim($_GET['email']);
-    $firstName = trim($_GET['first_name']);
-    $lastName = trim($_GET['last_name']);
-    $age = trim($_GET['age']);
+    $info = [
+        'first_name' => '',
+        'last_name' => '',
+        'email' => '',
+        'age' => '',
+    ];
 
-    if(file_exists($email . '.txt'))
+    foreach ($info as $key => &$value)
+        $value = $_GET[$key];
+
+    if (!isset($info['email'])) 
     {
-        fopen($email . '.txt', 'w');
+        echo 'Необходимо ввести ключ email.';
+    }
+    elseif ($info['email'] === '') {
+        echo 'В ключе email не указано значение.';
     }
     else
     {
-        $user = fopen($email . '.txt', 'w');
-        $text = $firstName . PHP_EOL . $lastName;
-        fwrite($user, $text);
-        fclose($user);
+        $fileName = './data/'.$info['email'].'.txt';
+  
+        if (file_exists($fileName)) 
+        {
+            $infoFile = file($fileName);
+            if (! isset($info['first_name']))
+                $info['first_name'] = trim(substr($infoFile[0], strpos($infoFile[0], ':') + 1));
+            if (! isset($info['last_name']))
+                $info['last_name'] = trim(substr($infoFile[1], strpos($infoFile[1], ':') + 1));
+            if (! isset($info['age']))
+                $info['age'] = trim(substr($infoFile[3], strpos($infoFile[3], ':') + 1));
+        }
+  
+        $infoStr = '';
+        $infoStr = $infoStr . 'First Name: ' . $info['first_name'] . PHP_EOL;
+        $infoStr = $infoStr . 'Last Name: ' . $info['last_name'] . PHP_EOL;
+        $infoStr = $infoStr . 'Email: ' . $info['email'] . PHP_EOL;
+        $infoStr = $infoStr . 'Age: ' . $info['age'];
+  
+        $successWriting = file_put_contents($fileName, $infoStr, 0);
+        if ($successWriting) 
+        {
+            echo 'Данные записаны в файл ' . $fileName . ' !!!';
+        }
+        else 
+        {
+            echo 'Ошибка записи в файл ' . $fileName . ' !!!';
+        }
     }
-
-
-
-
-    echo $email, PHP_EOL;
-    echo $firstName, PHP_EOL;
-    echo $lastName, PHP_EOL;
-    echo $age, PHP_EOL;
 ?>
