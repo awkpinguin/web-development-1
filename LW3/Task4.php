f1cc9f667ad54de2c5eed7e12e75702883455de9
Alexander Klochko <90823272+zernvaka@users.noreply.github.com>
2022-03-06 11:08:52 +0300

    Update Task4.php

--- LW3/Task4.php
+++ LW3/Task4.php
@@ -7,28 +7,28 @@
         'age' => '',
     ];
 
-    foreach ($info as $key => &$value)
+    foreach($info as $key => &$value)
         $value = $_GET[$key];
 
-    if (!isset($info['email'])) 
+    if(!isset($info['email'])) 
     {
         echo 'Необходимо ввести ключ email.';
     }
-    elseif ($info['email'] === '') {
+    elseif($info['email'] === '') {
         echo 'В ключе email не указано значение.';
     }
     else
     {
         $fileName = './data/'.$info['email'].'.txt';
   
-        if (file_exists($fileName)) 
+        if(file_exists($fileName)) 
         {
             $infoFile = file($fileName);
-            if (! isset($info['first_name']))
+            if(!isset($info['first_name']))
                 $info['first_name'] = trim(substr($infoFile[0], strpos($infoFile[0], ':') + 1));
-            if (! isset($info['last_name']))
+            if(!isset($info['last_name']))
                 $info['last_name'] = trim(substr($infoFile[1], strpos($infoFile[1], ':') + 1));
-            if (! isset($info['age']))
+            if(!isset($info['age']))
                 $info['age'] = trim(substr($infoFile[3], strpos($infoFile[3], ':') + 1));
         }
   
@@ -39,6 +39,7 @@
         $infoStr = $infoStr . 'Age: ' . $info['age'];
   
         $successWriting = file_put_contents($fileName, $infoStr, 0);
+        
         if ($successWriting) 
         {
             echo 'Данные записаны в файл ' . $fileName . ' !!!';
